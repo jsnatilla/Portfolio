6f0dbf527bf8d7a337f7fd1908abe93c1efbbb59
jsnatilla <62628543+jsnatilla@users.noreply.github.com>
2022-10-04 12:42:03 -0400

    Add files via upload

---
+++ css/colors.php
@@ -0,0 +1,48 @@
+<?php
+
+$princetonOrange: #ee8434ff;
+$indianRed: #c95d63ff;
+$englishLavender: #ae8799ff;
+$glaucous: #717ec3ff;
+$royal-blue-light: #496ddbff;
+
+?>
+
+<?php /* CSS HEX */
+--jonquil: #f9c80eff;
+--orange-red: #f86624ff;
+--imperial-red: #ea3546ff;
+--rebecca-purple: #662e9bff;
+--maximum-blue-green: #43bccdff;
+
+/* CSS HSL */
+--jonquil: hsla(47, 95%, 52%, 1);
+--orange-red: hsla(19, 94%, 56%, 1);
+--imperial-red: hsla(354, 81%, 56%, 1);
+--rebecca-purple: hsla(271, 54%, 39%, 1);
+--maximum-blue-green: hsla(187, 58%, 53%, 1);
+
+/* SCSS HEX */
+$jonquil: #f9c80eff;
+$orange-red: #f86624ff;
+$imperial-red: #ea3546ff;
+$rebecca-purple: #662e9bff;
+$maximum-blue-green: #43bccdff;
+
+/* SCSS RGB */
+$jonquil: rgba(249, 200, 14, 1);
+$orange-red: rgba(248, 102, 36, 1);
+$imperial-red: rgba(234, 53, 70, 1);
+$rebecca-purple: rgba(102, 46, 155, 1);
+$maximum-blue-green: rgba(67, 188, 205, 1);
+
+/* SCSS Gradient */
+$gradient-top: linear-gradient(0deg, #f9c80eff, #f86624ff, #ea3546ff, #662e9bff, #43bccdff);
+$gradient-right: linear-gradient(90deg, #f9c80eff, #f86624ff, #ea3546ff, #662e9bff, #43bccdff);
+$gradient-bottom: linear-gradient(180deg, #f9c80eff, #f86624ff, #ea3546ff, #662e9bff, #43bccdff);
+$gradient-left: linear-gradient(270deg, #f9c80eff, #f86624ff, #ea3546ff, #662e9bff, #43bccdff);
+$gradient-top-right: linear-gradient(45deg, #f9c80eff, #f86624ff, #ea3546ff, #662e9bff, #43bccdff);
+$gradient-bottom-right: linear-gradient(135deg, #f9c80eff, #f86624ff, #ea3546ff, #662e9bff, #43bccdff);
+$gradient-top-left: linear-gradient(225deg, #f9c80eff, #f86624ff, #ea3546ff, #662e9bff, #43bccdff);
+$gradient-bottom-left: linear-gradient(315deg, #f9c80eff, #f86624ff, #ea3546ff, #662e9bff, #43bccdff);
+$gradient-radial: radial-gradient(#f9c80eff, #f86624ff, #ea3546ff, #662e9bff, #43bccdff); ?>
